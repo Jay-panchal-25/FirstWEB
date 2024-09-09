c279d5752f5852a099a1f1cb5ba89ebdfc8971c9
Jay panchal <jaypanchal06334@gmail.com>
2024-09-09 11:41:26 +0530

    reponsive complete

--- assets/JS/theme.js
+++ assets/JS/theme.js
@@ -1,7 +1,7 @@
 var burger = document.querySelector(".burger");
 var middle = document.querySelector(".middle");
 var rightN = document.querySelector(".rightN");
-
+//It is for Responsive Navigation bar
 burger.addEventListener("click", () => {
   middle.classList.toggle("hidden");
   rightN.classList.toggle("hidden");
